d1cdd84af14ac7d15e89a9dc814f0108246e0ac7
028marakesh <b.loganov@list.ru>
2022-09-07 17:00:24 +0300

    '

---
+++ Loganov_aa2107.cpp
@@ -0,0 +1,12 @@
+// Loganov_aa2107.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int a;
+    std::cin >> a;
+    std::cout << "Hello World!\n";
+}
+
